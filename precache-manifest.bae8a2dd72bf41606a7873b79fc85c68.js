a58289918928f56244ce65300804762962a1c06f
Pythonsway <adam@pythonsway.it>
2019-07-07 12:52:10 +0200

    Updates

---
+++ precache-manifest.bae8a2dd72bf41606a7873b79fc85c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d787f64fd74a606690be3e33bd3a2db0",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "53617fd0defba641056d",
+    "url": "/markdown-previewer/static/css/main.c441747b.chunk.css"
+  },
+  {
+    "revision": "2dee4bcc5ef60234e20b",
+    "url": "/markdown-previewer/static/js/2.7592794e.chunk.js"
+  },
+  {
+    "revision": "53617fd0defba641056d",
+    "url": "/markdown-previewer/static/js/main.ca44fda3.chunk.js"
+  },
+  {
+    "revision": "6ac31481741a1aafa92f",
+    "url": "/markdown-previewer/static/js/runtime~main.2b49566a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/markdown-previewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
